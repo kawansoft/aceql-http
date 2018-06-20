7d51d49c14a5895ed69efc8f66897dc796ad4a9f
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2018-06-20 09:53:43 +0200

    Add new classes for stored procedures

---
+++ src/main/java/org/kawanfw/sql/servlet/sql/ParameterDirection.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.kawanfw.sql.servlet.sql;
+
+/**
+ * @author Nicolas de Pomereu
+ *
+ */
+public class ParameterDirection {
+    public static final String IN = "in";
+    public static final String OUT = "out";
+    public static final String INOUT = "inout";
+}
