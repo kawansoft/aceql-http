996d100376add84292542f4b9aa94a948a02d294
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2020-02-22 12:51:19 +0100

    Add SshUserAuthenticator

---
+++ src/main/java/org/kawanfw/sql/api/server/SshUserAuthenticator.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package org.kawanfw.sql.api.server;
+
+/**
+ * @author Nicolas de Pomereu
+ *
+ */
+public class SshUserAuthenticator {
+
+    /**
+     * Constructor.
+     */
+    public SshUserAuthenticator() {
+
+    }
+
+}
