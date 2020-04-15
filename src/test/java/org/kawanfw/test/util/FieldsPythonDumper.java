c1f58f04c83cc837271c7b586222929376e1760c
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2020-04-15 14:37:17 +0200

    Remove unused class

---
+++ src/test/java/org/kawanfw/test/util/FieldsPythonDumper.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.kawanfw.test.util;
+
+/**
+ * @author Nicolas de Pomereu
+ *
+ */
+public class FieldsDumper {
+
+    /**
+     * 
+     */
+    public FieldsDumper() {
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
