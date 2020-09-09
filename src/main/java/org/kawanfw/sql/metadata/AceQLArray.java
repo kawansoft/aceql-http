6ee0fffd86a1930f9c44515621c972c6badbccbc
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2020-09-09 20:06:51 +0200

    Clean SQL Array implementation

--- src/main/java/org/kawanfw/sql/metadata/AceQLArray.java
+++ src/main/java/org/kawanfw/sql/metadata/AceQLArray.java
@@ -24,10 +24,14 @@
  */
 package org.kawanfw.sql.metadata;
 
+import java.math.BigDecimal;
 import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.Date;
 import java.util.Map;
 
 import org.kawanfw.sql.jdbc.metadata.ArrayTransporter;
@@ -46,9 +50,8 @@ public class AceQLArray implements Array {
     private int baseType;
     private String arrayAsJoin;
 
-
     /**
-     * Necessary void constructor for JSON
+     * Necessary void constructor for JSON.
      */
     public AceQLArray() {
 
@@ -64,8 +67,39 @@ public class AceQLArray implements Array {
 
 	this.baseTypeName = array.getBaseTypeName();
 	this.baseType = array.getBaseType();
-	String[] stringArray = (String[]) array.getArray();
-	this.arrayAsJoin = ArrayTransporter.arrayToString(stringArray);
+
+	// Date & time are converted to long value, to avoid string representation
+	if (baseType == Types.DATE) {
+	    Date[] objectArray = (Date[]) array.getArray();
+	    String[] stringArray = new String[objectArray.length];
+	    for (int i = 0; i < objectArray.length; i++) {
+		stringArray[i] = "" + objectArray[i].getTime();
+	    }
+	}
+	else if (baseType == Types.TIMESTAMP) {
+	    Timestamp[] objectArray = (Timestamp[]) array.getArray();
+	    String[] stringArray = new String[objectArray.length];
+	    for (int i = 0; i < objectArray.length; i++) {
+		stringArray[i] = "" + objectArray[i].getTime();
+	    }
+	}
+	else if (baseType == Types.TIME) {
+	    Timestamp[] objectArray = (Timestamp[]) array.getArray();
+	    String[] stringArray = new String[objectArray.length];
+	    for (int i = 0; i < objectArray.length; i++) {
+		stringArray[i] = "" + objectArray[i].getTime();
+	    }
+	}
+	else {
+	    Object[] objectArray = (Object[]) array.getArray();
+	    String[] stringArray = new String[objectArray.length];
+	    for (int i = 0; i < objectArray.length; i++) {
+		stringArray[i] = objectArray[i].toString();
+	    }
+
+	    this.arrayAsJoin = ArrayTransporter.arrayToString(stringArray);
+	}
+
     }
 
     /*
@@ -95,7 +129,170 @@ public class AceQLArray implements Array {
      */
     @Override
     public Object getArray() throws SQLException {
-	return ArrayTransporter.stringToStringArray(arrayAsJoin);
+	String[] stringArray =  ArrayTransporter.stringToStringArray(arrayAsJoin);
+	if (stringArray == null) {
+	    return null;
+	}
+
+	/**
+	 * <pre><code>
+	This is a PostgreSQL example:
+        Java Type	Supported binary 	PostgreSQL™ Types	Default PostgreSQL™ Type
+        short[], 	Short[]				int2[]				int2[]
+        int[],		Integer[]			int4[]				int4[]
+        long[], 	Long[]				int8[]				int8[]
+        float[], 	Float[]				float4[]			float4[]
+        double[], 	Double[]			float8[]			float8[]
+        boolean[], 	Boolean[]			bool[]				bool[]
+        String[]	varchar[], 			text[]				varchar[]
+
+	https://www.cis.upenn.edu/~bcpierce/courses/629/jdkdocs/guide/jdbc/getstart/mapping.doc.html
+	CHAR			String
+	VARCHAR			String
+	LONGVARCHAR		String
+
+	NUMERIC			java.math.BigDecimal
+	DECIMAL			java.math.BigDecimal
+
+	BIT			boolean
+	TINYINT			byte
+
+	SMALLINT		short
+	INTEGER			int
+
+	BIGINT			long
+	REAL			float
+	FLOAT			double
+	DOUBLE			double
+
+	BINARY			byte[]		NOT SUPPORTED
+	VARBINARY		byte[]		NOT SUPPORTED
+	LONGVARBINARY		byte[]		NOT SUPPORTED
+
+	DATE			java.sql.Date
+	TIME			java.sql.Time
+	TIMESTAMP		java.sql.Timestamp
+        </code></pre>
+	 */
+
+	if (isString(baseType)) {
+	    return stringArray;
+	}
+	else if (isBigDecimal(baseType)) {
+	    BigDecimal[] theArray = new BigDecimal[stringArray.length];
+	    for (int i = 0; i < stringArray.length; i++) {
+		theArray[i] = new BigDecimal(stringArray[i]);
+	    }
+	    return theArray;
+	}
+	else if (isBoolean(baseType)) {
+	    Boolean[] theArray = new Boolean[stringArray.length];
+	    for (int i = 0; i < stringArray.length; i++) {
+		theArray[i] = Boolean.parseBoolean(stringArray[i]);
+	    }
+	    return theArray;
+	}
+	else if (isShort(baseType)) {
+	    Short[] theArray = new Short[stringArray.length];
+	    for (int i = 0; i < stringArray.length; i++) {
+		theArray[i] = Short.parseShort(stringArray[i]);
+	    }
+	    return theArray;
+	}
+	else if (isInt(baseType)) {
+	    Integer[] theArray = new Integer[stringArray.length];
+	    for (int i = 0; i < stringArray.length; i++) {
+		theArray[i] = Integer.parseInt(stringArray[i]);
+	    }
+	    return theArray;
+	}
+	else if (isLong(baseType)) { // Includes date
+	    Long[] theArray = new Long[stringArray.length];
+	    for (int i = 0; i < stringArray.length; i++) {
+		theArray[i] = Long.parseLong(stringArray[i]);
+	    }
+	    return theArray;
+	}
+	else if (isFloat(baseType)) {
+	    Float[] theArray = new Float[stringArray.length];
+	    for (int i = 0; i < stringArray.length; i++) {
+		theArray[i] = Float.parseFloat(stringArray[i]);
+	    }
+	    return theArray;
+	}
+	else if (isDouble(baseType)) {
+	    Double[] theArray = new Double[stringArray.length];
+	    for (int i = 0; i < stringArray.length; i++) {
+		theArray[i] = Double.parseDouble(stringArray[i]);
+	    }
+	    return theArray;
+	}
+	else {
+	    return stringArray;
+	}
+
+    }
+
+    /** <pre><code>
+	CHAR			String
+	VARCHAR			String
+	LONGVARCHAR		String
+
+	NUMERIC			java.math.BigDecimal
+	DECIMAL			java.math.BigDecimal
+
+	BIT			boolean
+	TINYINT			byte
+
+	SMALLINT		short
+	INTEGER			int
+
+	BIGINT			long
+	REAL			float
+	FLOAT			double
+	DOUBLE			double
+
+	BINARY			byte[]		NOT SUPPORTED
+	VARBINARY		byte[]		NOT SUPPORTED
+	LONGVARBINARY		byte[]		NOT SUPPORTED
+
+	DATE			java.sql.Date
+	TIME			java.sql.Time
+	TIMESTAMP		java.sql.Timestamp
+	</code></pre>
+*/
+
+    private boolean isShort(int baseType) {
+	return baseType == Types.SMALLINT;
+    }
+
+    // Includes date & time
+    private boolean isLong(int baseType) {
+	return baseType == Types.BIGINT || baseType == Types.DATE || baseType == Types.TIMESTAMP || baseType == Types.TIME;
+    }
+
+    private boolean isBoolean(int baseType) {
+	return baseType == Types.BOOLEAN;
+    }
+
+    private boolean isFloat(int baseType) {
+	return baseType == Types.REAL;
+    }
+
+    private boolean isBigDecimal(int baseType) {
+	return baseType == Types.NUMERIC || baseType == Types.DECIMAL;
+    }
+
+    private boolean isDouble(int baseType) {
+	return baseType == Types.REAL || baseType == Types.FLOAT;
+    }
+
+    private boolean isInt(int baseType) {
+	return baseType == Types.INTEGER;
+    }
+
+    private boolean isString(int baseType) {
+	return baseType == Types.CHAR || baseType == Types.VARCHAR || baseType == Types.LONGVARCHAR;
     }
 
     /*
