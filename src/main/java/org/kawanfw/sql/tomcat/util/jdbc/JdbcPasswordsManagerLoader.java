b26bb5e03af92154f0abed30dd19e19733b8cdb2
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-04-17 18:41:41 +0200

    Clean JdbcPasswordsManagerLoader Javadoc

--- src/main/java/org/kawanfw/sql/tomcat/util/jdbc/JdbcPasswordsManagerLoader.java
+++ src/main/java/org/kawanfw/sql/tomcat/util/jdbc/JdbcPasswordsManagerLoader.java
@@ -1,3 +1,28 @@
+/*
+ * This file is part of AceQL HTTP.
+ * AceQL HTTP: SQL Over HTTP                                     
+ * Copyright (C) 2020,  KawanSoft SAS
+ * (http://www.kawansoft.com). All rights reserved.                                
+ *                                                                               
+ * AceQL HTTP is free software; you can redistribute it and/or                 
+ * modify it under the terms of the GNU Lesser General Public                    
+ * License as published by the Free Software Foundation; either                  
+ * version 2.1 of the License, or (at your option) any later version.            
+ *                                                                               
+ * AceQL HTTP is distributed in the hope that it will be useful,               
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU             
+ * Lesser General Public License for more details.                               
+ *                                                                               
+ * You should have received a copy of the GNU Lesser General Public              
+ * License along with this library; if not, write to the Free Software           
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  
+ * 02110-1301  USA
+ * 
+ * Any modifications to this file must keep this entire header
+ * intact.
+ */
+
 package org.kawanfw.sql.tomcat.util.jdbc;
 
 import java.io.IOException;
@@ -8,14 +33,26 @@ import java.util.Properties;
 
 import org.kawanfw.sql.api.server.DatabaseConfigurationException;
 import org.kawanfw.sql.api.server.auth.jdbc.JdbcPasswordsManager;
-import org.kawanfw.sql.util.Tag;
 
+/**
+ * Calls a concrete JdbcPasswordsManager.getPassword(database) if defined inaceql-server.properties.
+ * @author Nicolas de Pomereu
+ *
+ */
 public class JdbcPasswordsManagerLoader {
 
 
+    /**
+     * Gets the password for the passed database from JdbcPasswordsManager, if concrete implementation is defined in properties
+     * @param database
+     * @param properties
+     * @return
+     * @throws IOException
+     * @throws SQLException
+     */
     public static char [] getPasswordUsingJdbcPasswordManagers(String database, Properties properties) throws IOException, SQLException {
 	Objects.requireNonNull(database, "database cannot be null!");
-	Objects.requireNonNull(database, "database cannot be null!");
+	Objects.requireNonNull(properties, "properties cannot be null!");
 	
 	String jdbcPasswordsManagerClassName = properties.getProperty("jdbcPasswordsManagerClassName");
 	
