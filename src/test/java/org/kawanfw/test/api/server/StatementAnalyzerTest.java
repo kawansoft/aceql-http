07c8ae54d80e877157688f3883d40b57455d1f0c
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-05-17 18:08:18 +0200

    pom.xml uses new Maven Compiler

--- src/test/java/org/kawanfw/test/api/server/StatementAnalyzerTest.java
+++ src/test/java/org/kawanfw/test/api/server/StatementAnalyzerTest.java
@@ -29,7 +29,7 @@ import java.util.List;
 import java.util.Vector;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.kawanfw.sql.api.server.StatementAnalyzer;
 
 public class StatementAnalyzerTest {
