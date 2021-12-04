d952b8f483791a5b9d313f4eee8ab64a10ee69ca
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-12-04 18:17:57 +0100

    QueryExecutor creation

---
+++ src/main/java/org/kawanfw/sql/api/server/QueryExecutor.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.kawanfw.sql.api.server;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Allows client side to call a server side programmed class that returns a {@code ResultSet}.
+ * This a kind of stored procedure in Java.
+ * <br><br>
+ * @author Nicolas de Pomereu
+ *
+ */
+public interface QueryExecutor {
+    
+    /**
+     * Executes a a query and return {@code ResultSet}.
+     * @param args	the parameters to pass to the method
+     * @return a <code>ResultSet</code> object that contains the data produced by the
+     *         query; never <code>null</code>
+     * @throws IOException
+     * @throws IOException           if an IOException occurs
+     */
+    public ResultSet executeQuery(String[] args) throws IOException, SQLException;
+
+}
