07bbc159ff9eff6ad12d245c919381af1a693afd
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-12-09 17:23:53 +0100

    RemoteQueryExecutor! Clean Javadoc

--- src/main/java/org/kawanfw/sql/api/server/executor/RemoteQueryExecutor.java
+++ src/main/java/org/kawanfw/sql/api/server/executor/RemoteQueryExecutor.java
@@ -8,20 +8,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * Allows client side to call a server side programmed class that returns a {@code ResultSet}.
- * This a kind of stored procedure in Java.
+ * Allows client side to call a server side programmed class that returns a
+ * {@code ResultSet}. This a kind of stored procedure in Java. <br>
  * <br>
+ * Client usage is limited to the Client JDBC Driver for this version.<br>
+ * 
  * @author Nicolas de Pomereu
  * @since 9.1
  *
  */
 public interface RemoteQueryExecutor {
-    
+
     /**
      * Executes a a query and return {@code ResultSet}.
-     * @param args	the parameters to pass to the method
-     * @return a <code>ResultSet</code> object that contains the data produced by the
-     *         query; never <code>null</code>
+     * 
+     * @param args the parameters to pass to the method
+     * @return a <code>ResultSet</code> object that contains the data produced by
+     *         the query; never <code>null</code>
      * @throws IOException  if an IOException occurs
      * @throws SQLException if a SQLException occurs
      */
