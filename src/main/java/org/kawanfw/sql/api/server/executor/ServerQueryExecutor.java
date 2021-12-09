1b5b71564cce14ac695862a6d78e9003e442286a
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-12-09 18:09:41 +0100

    ServerQueryExecutor: add connection param

--- src/main/java/org/kawanfw/sql/api/server/executor/ServerQueryExecutor.java
+++ src/main/java/org/kawanfw/sql/api/server/executor/ServerQueryExecutor.java
@@ -4,6 +4,7 @@
 package org.kawanfw.sql.api.server.executor;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -22,13 +23,16 @@ public interface ServerQueryExecutor {
     /**
      * Executes a a query and return {@code ResultSet}.
      * 
-     * @param username  the client username that asks for the query. Allows to perform a security check.
-     * @param args 	the parameters passed by the client side.
+     * @param username   the client username that asks for the query. Allows to
+     *                   perform a security check.
+     * @param connection The current SQL/JDBC <code>Connection</code>
+     * @param args       the parameters passed by the client side.
      * @return a <code>ResultSet</code> object that contains the data produced by
      *         the query; never <code>null</code>
      * @throws IOException  if an IOException occurs
      * @throws SQLException if a SQLException occurs
      */
-    public ResultSet executeQuery(String username, Object... params) throws IOException, SQLException;
+    public ResultSet executeQuery(String username, Connection connection, Object... params)
+	    throws IOException, SQLException;
 
 }
