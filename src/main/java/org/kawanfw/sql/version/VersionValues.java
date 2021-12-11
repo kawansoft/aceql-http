b60ed48477a79435e60a986604d9eb049fd5b31f
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-12-11 12:01:30 +0100

    VersionValues: update version date

--- src/main/java/org/kawanfw/sql/version/VersionValues.java
+++ src/main/java/org/kawanfw/sql/version/VersionValues.java
@@ -30,6 +30,6 @@ package org.kawanfw.sql.version;
 
 public class VersionValues {
     public static final String VERSION = "v9.1";
-    public static final String DATE = "10-Dec-2021";
+    public static final String DATE = "11-Dec-2021";
 }
 
