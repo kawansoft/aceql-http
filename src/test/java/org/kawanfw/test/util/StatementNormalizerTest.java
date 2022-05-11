8fd4b8fe88746b6e3216523223dd0e398ffaa013
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-05-11 15:20:24 +0200

    StatementNormalizerTest clean

--- src/test/java/org/kawanfw/test/util/StatementNormalizerTest.java
+++ src/test/java/org/kawanfw/test/util/StatementNormalizerTest.java
@@ -24,6 +24,7 @@
  */
 package org.kawanfw.test.util;
 
+import org.junit.Assert;
 import org.kawanfw.sql.api.server.StatementNormalizer;
 
 /**
@@ -38,19 +39,25 @@ public class StatementNormalizerTest {
      * @param args
      */
     public static void main(String[] args) {
-//	String sql = "    SELECT 	*         " + CR_LF + " from     my_table 	where      my_colum   =   ? ";
-//	System.out.println("sql           : " + sql);
-//	System.out.println("normalized sql: " + StatementNormalizer.getNormalizedText(sql));
-//	System.out.println();
-//	System.out.println();
 	
 	//String str = "This is a      " + CR_LF + "    string that         will be highlighted when your '   1 regular  2 expression  3 .'  text '.  4 regular  5 expression  6 .'   matches something.";
-	String sql = "    SELECT 	*         " + CR_LF + " from     my_table 	where      my_colum   =   ? and name = 'John Doe' ";
-	System.out.println(sql);
+	final String sql1 = "    SELECT 	*         " + CR_LF + " from     my_table 	where      my_colum   =   ? and name = 'John Doe' ";
+	System.out.println("sql1: " + sql1);
+	
+	final String sql1Normalized = StatementNormalizer.getNormalized(sql1);
+	System.out.println(sql1Normalized);
+	
+	System.out.println();
 	System.out.println();
 	
-	System.out.println(StatementNormalizer.getNormalized(sql));
-
+	final String sql2 = "SELECT 	 * from " + CR_LF + "    my_table  where      my_colum   = ? and name = 'John Doe' ";
+	System.out.println("sql2: " + sql2);
+	final String sql2Normalized = StatementNormalizer.getNormalized(sql2);
+	System.out.println(sql2Normalized);
+	
+	System.out.println();
+	Assert.assertEquals("sql1Normalized equals sql2Normalized", sql1Normalized, sql2Normalized);
+	System.out.println("sql1Normalized equals sql2Normalized!");
     }
 
 
