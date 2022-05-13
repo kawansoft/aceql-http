1f51de9e835dcaf8a7d4f42d0a2e31e97f7492bb
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-05-13 18:08:34 +0200

    SqlFirewallsCreator: add DenyExceptOnWhitelistManager

--- src/main/java/org/kawanfw/sql/servlet/injection/classes/creator/SqlFirewallsCreator.java
+++ src/main/java/org/kawanfw/sql/servlet/injection/classes/creator/SqlFirewallsCreator.java
@@ -38,9 +38,10 @@ import org.kawanfw.sql.api.server.firewall.DefaultSqlFirewallManager;
 import org.kawanfw.sql.api.server.firewall.DenyDatabaseWriteManager;
 import org.kawanfw.sql.api.server.firewall.DenyDclManager;
 import org.kawanfw.sql.api.server.firewall.DenyDdlManager;
+import org.kawanfw.sql.api.server.firewall.DenyExceptOnWhitelistManager;
 import org.kawanfw.sql.api.server.firewall.DenyMetadataQueryManager;
-import org.kawanfw.sql.api.server.firewall.DenyStatementClassManager;
 import org.kawanfw.sql.api.server.firewall.DenyOnBlacklistManager;
+import org.kawanfw.sql.api.server.firewall.DenyStatementClassManager;
 import org.kawanfw.sql.api.server.firewall.SqlFirewallManager;
 import org.kawanfw.sql.util.FrameworkDebug;
 
@@ -75,8 +76,9 @@ public class SqlFirewallsCreator {
     private static String[] PREDEFINED_CLASS_NAMES = { CsvRulesManager.class.getSimpleName(),
 	    CsvRulesManagerNoReload.class.getSimpleName(), DefaultSqlFirewallManager.class.getSimpleName(),
 	    DenyDclManager.class.getSimpleName(), DenyDdlManager.class.getSimpleName(),
-	    DenyDatabaseWriteManager.class.getSimpleName(), DenyOnBlacklistManager.class.getSimpleName(),
-	    DenyMetadataQueryManager.class.getSimpleName(), DenyStatementClassManager.class.getSimpleName(), };
+	    DenyDatabaseWriteManager.class.getSimpleName(), DenyExceptOnWhitelistManager.class.getSimpleName(),
+	    DenyOnBlacklistManager.class.getSimpleName(), DenyMetadataQueryManager.class.getSimpleName(),
+	    DenyStatementClassManager.class.getSimpleName(), };
 
     private List<String> sqlFirewallClassNames = new ArrayList<>();
     private List<SqlFirewallManager> sqlFirewallManagers = new ArrayList<>();
