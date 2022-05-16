e9ad9312c4599cd17e4eddf43a1de98edebc8012
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-05-16 17:19:02 +0200

    SqlFirewallManager Clean

--- src/main/java/org/kawanfw/sql/api/server/firewall/SqlFirewallManager.java
+++ src/main/java/org/kawanfw/sql/api/server/firewall/SqlFirewallManager.java
@@ -80,10 +80,10 @@ import org.kawanfw.sql.api.server.StatementAnalyzer;
  * <li>{@link DenyMetadataQueryManager}: manager that denies the use of the
  * AceQL Metadata Query API.</li>
  * <li>{@link DenyOnBlacklistManager}: manager that denies statements that are
- * listed in a blacklist text file.</li> *
+ * listed in a blacklist text file.</li>
  * <li>{@link DenySqlInjectionManager}: firewall manager that allows detecting
- * SQL injection attacks, using a third-party API:
- * <a href="https://www.cloudmersive.com">www.cloudmersive.com</a>.</li>
+ * SQL injection attacks, using <a href="https://www.cloudmersive.com">Cloudmersive</a> third-party API:
+ * .</li>
  * <li>{@link DenyStatementClassManager}: manager that denies any call of the
  * raw Statement Java class. (Calling Statements without parameters is
  * forbidden).</li>
