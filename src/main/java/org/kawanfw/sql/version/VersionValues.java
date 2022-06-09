4098d5648859fb2a828b93dac54b7f15e345ecf0
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-06-09 10:30:49 +0200

    Update version date

--- src/main/java/org/kawanfw/sql/version/VersionValues.java
+++ src/main/java/org/kawanfw/sql/version/VersionValues.java
@@ -30,6 +30,6 @@ package org.kawanfw.sql.version;
 
 class VersionValues {
     public static final String VERSION = "v11.0";
-    public static final String DATE = "31-May-2022";
+    public static final String DATE = "09-Jun-2022";
 }
 
