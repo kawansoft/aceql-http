f8558a1d1df27a520d454f943f7942c14dc753ed
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-09-01 15:38:57 +0200

    Update files BSL Header

--- src/main/java/org/kawanfw/sql/api/server/logging/LoggerCreator.java
+++ src/main/java/org/kawanfw/sql/api/server/logging/LoggerCreator.java
@@ -1,37 +1,13 @@
 /*
- * This file is part of KawanWall.
- * KawanWall: Firewall for SQL statements
- * Copyright (C) 2022,  KawanSoft SAS
- * (http://www.kawansoft.com). All rights reserved.                 
- *                                                                         
- * KawanWall is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as 
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.         
- *              
- * KawanWall is distributed in the hope that it will be useful,               
- * but WITHOUT ANY WARRANTY; without even the implied warranty of                
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU Affero General Public License for more details.       
- *                                  
- * You should have received a copy of the GNU Affero General 
- * Public License along with this program; if not, see 
- * <http://www.gnu.org/licenses/>.
- *
- * If you develop commercial activities using KawanWall, you must: 
- * a) disclose and distribute all source code of your own product,
- * b) license your own product under the GNU General Public License.
+ * Copyright (c)2022 KawanSoft S.A.S. All rights reserved.
  * 
- * You can be released from the requirements of the license by
- * purchasing a commercial license. Buying such a license will allow you 
- * to ship KawanWall with your closed source products without disclosing 
- * the source code.
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file in the project's root directory.
  *
- * For more information, please contact KawanSoft SAS at this
- * address: sales@kawansoft.com
- * 
- * Any modifications to this file must keep this entire header
- * intact.
+ * Change Date: 2026-09-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2.0 of the Apache License.
  */
 package org.kawanfw.sql.api.server.logging;
 
--- src/main/java/org/kawanfw/sql/tomcat/ProEditionServletNamesGetterWrap.java
+++ src/main/java/org/kawanfw/sql/tomcat/ProEditionServletNamesGetterWrap.java
@@ -4,12 +4,11 @@
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file in the project's root directory.
  *
- * Change Date: 2027-08-31
+ * Change Date: 2026-09-01
  *
  * On the date above, in accordance with the Business Source License, use
  * of this software will be governed by version 2.0 of the Apache License.
  */
-
 package org.kawanfw.sql.tomcat;
 
 import java.sql.SQLException;
