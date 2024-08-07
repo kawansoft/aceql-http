bc855adef67d8f8e482d9d9247db93112370da17
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2024-08-07 19:00:05 +0200

    12.5 everywhere in Javadoc

--- src/main/java/org/kawanfw/sql/api/server/web/WebServerApi.java
+++ src/main/java/org/kawanfw/sql/api/server/web/WebServerApi.java
@@ -27,7 +27,7 @@ import org.kawanfw.sql.api.util.webserver.WebServerApiWrapper;
 * Note that the {@code org.kawanfw.sql.WebServer} class is used to start and
 * stop the embedded Web Server from command line and uses the APIs of this
 * class. Source code is available here:&nbsp; <a href=
-* "https://docs.aceql.com/rest/soft/12.3/src/WebServer.java">WebServer.java</a>.
+* "https://docs.aceql.com/rest/soft/12.5/src/WebServer.java">WebServer.java</a>.
 *
 * @author Nicolas de Pomereu
 *
